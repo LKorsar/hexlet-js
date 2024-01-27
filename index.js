2b1db72fc8eec1c7f8b358c7ac11d9c9710a00ba
Мария Акимкина <wapovalova@gmail.com>
2024-01-27 21:34:16 +0300

    add eslintrc.yml, update index.js, src/index.js, package.json and package-lock.json

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-console.log("Hello, Hexlet!");
-import getLast from './src/index.js';
-getLast(["one", "two"]);
+import getLast from './src/index';
+
+console.log('Hello, Hexlet!');
+getLast(['one', 'two']);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import _ from "lodash";
+import _ from 'lodash';
+
 export default (coll) => {
-console.log(_.last(coll));
+  console.log(_.last(coll));
 };
